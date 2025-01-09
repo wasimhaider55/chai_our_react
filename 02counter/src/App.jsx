c0612db1357ef56645fea992b5eab8ebb3c79740
wasim haider <wasimhaider14379@gmail.com>
2025-01-09 13:18:27 +0500

    counter project with useState hook

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,34 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [count, setCount] = useState(10)
+  // add function
+  const addValue = () => {
+    if (count >= 20) {
+     return  
+    } else{
+      setCount(count + 1); 
+    }
+  }
+// Remove Function
+  const removeValue = () => {
+    if(count == 0){
+      return
+    }else{
+      setCount(count - 1);
+    }
+  
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+   <h1> Chai our Code </h1>
+   <h2>Counter value : {count} </h2>
+
+   <button onClick={addValue}>increase count {count} </button>
+   <br />
+   <br />
+   <button onClick={removeValue}>decrease count {count} </button>
+   </>
   )
 }
 
